7357f3401a462182ed9b8ff034d8571067f5f51d
kaiyaharuhiko <kaiya@kanagawa-u.ac.jp>
2020-12-14 23:52:20 +0900

    test3

---
+++ Test3/src/Player.java
@@ -0,0 +1,17 @@
+
+public class Player {
+	private int hand;
+	public int getHand() {
+		return hand;
+	}
+	public void setHand(int hand) {
+		this.hand = hand;
+	}
+	public int getWinCount() {
+		return winCount;
+	}
+	public void setWinCount(int winCount) {
+		this.winCount = winCount;
+	}
+	private int winCount;
+}
---
+++ Test3/src/PlayerTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void testGetHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWinCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetWinCount() {
+		fail("Not yet implemented");
+	}
+
+}
